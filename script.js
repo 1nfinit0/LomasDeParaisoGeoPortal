f242d122dee6cc7b5c7b9902a218f8be9262d5a7
tobi <noggnzzz@gmail.com>
2023-03-31 11:19:55 -0500

    menú desplegable listo

--- script.js
+++ script.js
@@ -4,25 +4,49 @@ const footer = document.getElementsByTagName('footer')[0];
 const logoContainer = document.querySelector('.logo-container');
 const logo = document.getElementsByClassName('logo-container')[0];
 
-header.style.display = 'none';
-main.style.display = 'none';
-footer.style.display = 'none';
 
-function beat() {
-  logoContainer.classList.add('beating');
-  setTimeout(() => {
-    logoContainer.classList.remove('beating');
-  }, 400);
-}
-setInterval(beat, 1000);
+// header.style.display = 'none';
+// main.style.display = 'none';
+// footer.style.display = 'none';
 
-setTimeout(() => {
-  logo.style.transition = 'opacity 1s ease-in-out';
-  logo.style.opacity = 0;
-}, 4000);
-setTimeout(() => {
-  header.style.display = 'block';
-  main.style.display = 'block';
-  footer.style.display = 'block';
-  logo.style.display = 'none';
-}, 5000);
+// function beat() {
+//   logoContainer.classList.add('beating');
+//   setTimeout(() => {
+//     logoContainer.classList.remove('beating');
+//   }, 400);
+// }
+// setInterval(beat, 1000);
+
+// setTimeout(() => {
+//   logo.style.transition = 'opacity 1s ease-in-out';
+//   logo.style.opacity = 0;
+// }, 4000);
+// setTimeout(() => {
+//   header.style.display = 'block';
+//   main.style.display = 'block';
+//   footer.style.display = 'block';
+//   logo.style.display = 'none';
+// }, 5000);
+
+
+const toggleBtn = document.querySelector('.toggle-btn');
+const svgIcon = document.querySelector('.icon');
+const iconInitial = document.querySelector('.icon-initial');
+const iconClose = document.querySelector('.icon-close');
+const checkbox = toggleBtn.querySelector('input[type="checkbox"]');
+const content = document.getElementsByClassName('menu-content')[0];
+
+checkbox.addEventListener('change', () => {
+  if (checkbox.checked) {
+    iconInitial.style.display = 'none';
+    iconClose.style.display = 'block';
+    content.style.transform = 'translateX(0)';
+    document.body.style.overflow = 'hidden'; // Desactivar el scroll vertical
+  } else {
+    iconInitial.style.display = 'block';
+    iconClose.style.display = 'none';
+    content.style.transform = 'translateX(-100%)';
+    document.body.style.overflow = ''; // Activar el scroll vertical
+  }
+});
+
