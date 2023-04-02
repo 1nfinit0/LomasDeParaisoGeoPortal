ae50e3d0140c916d3d346850879f97600d642112
ToshibaTobi <noggnzzz@gmail.com>
2023-04-01 22:54:45 -0500

    deploying the preload logo

--- script.js
+++ script.js
@@ -5,28 +5,28 @@ const logoContainer = document.querySelector('.logo-container');
 const logo = document.getElementsByClassName('logo-container')[0];
 
 
-// header.style.display = 'none';
-// main.style.display = 'none';
-// footer.style.display = 'none';
+header.style.display = 'none';
+main.style.display = 'none';
+footer.style.display = 'none';
 
-// function beat() {
-//   logoContainer.classList.add('beating');
-//   setTimeout(() => {
-//     logoContainer.classList.remove('beating');
-//   }, 400);
-// }
-// setInterval(beat, 1000);
+function beat() {
+  logoContainer.classList.add('beating');
+  setTimeout(() => {
+    logoContainer.classList.remove('beating');
+  }, 400);
+}
+setInterval(beat, 1000);
 
-// setTimeout(() => {
-//   logo.style.transition = 'opacity 1s ease-in-out';
-//   logo.style.opacity = 0;
-// }, 4000);
-// setTimeout(() => {
-//   header.style.display = 'block';
-//   main.style.display = 'block';
-//   footer.style.display = 'block';
-//   logo.style.display = 'none';
-// }, 5000);
+setTimeout(() => {
+  logo.style.transition = 'opacity 1s ease-in-out';
+  logo.style.opacity = 0;
+}, 4000);
+setTimeout(() => {
+  header.style.display = 'block';
+  main.style.display = 'block';
+  footer.style.display = 'block';
+  logo.style.display = 'none';
+}, 5000);
 
 
 const toggleBtn = document.querySelector('.toggle-btn');
