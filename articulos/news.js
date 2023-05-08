feff4c60d61cbc81b25e516afb059a04b2e95b7a
Tobi <noggnzzz@gmail.com>
2023-05-07 20:28:43 -0500

    noticias finishied

---
+++ articulos/news.js
@@ -0,0 +1,34 @@
+const news = [
+  {
+    id: 1,
+    href: '#',
+    imagenSrc: "./articulos/img/inventario_2023_voluntariado_pic.jpeg",
+    imagenAlt: 'Descripción de la imagen',
+    motivo: 'MOTIVO DE LA NOTICIA (MÁXIMO 7 PALABRAS EN MAYÚSCULAS)',
+    titulo: 'Título de la noticia, máximo de 23 palabras, se pueden expresar citas textuales o frases de la noticia.',
+    descripcion: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.',
+    contenido: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.',
+    },
+    {
+    id: 2,
+    href: '#',
+    imagenSrc: "./articulos/img/inventario_2023_voluntariado_pic.jpeg",
+    imagenAlt: 'Descripción de la imagen',
+    motivo: 'MOTIVO DE LA NOTICIA (MÁXIMO 7 PALABRAS EN MAYÚSCULAS)',
+    titulo: 'Título de la noticia, máximo de 23 palabras, se pueden expresar citas textuales o frases de la noticia.',
+    descripcion: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.',
+    contenido: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.',
+    },
+    {
+    id: 3,
+    href: '#',
+    imagenSrc: "./articulos/img/inventario_2023_voluntariado_pic.jpeg",
+    imagenAlt: 'Descripción de la imagen',
+    motivo: 'MOTIVO DE LA NOTICIA (MÁXIMO 7 PALABRAS EN MAYÚSCULAS)',
+    titulo: 'Título de la noticia, máximo de 23 palabras, se pueden expresar citas textuales o frases de la noticia.',
+    descripcion: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.',
+    contenido: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.',
+    },
+];
+
+export { news };
--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+import { news } from './articulos/news.js';
 const header = document.getElementsByTagName('header')[0];
 const main = document.getElementsByTagName('main')[0];
 const footer = document.getElementsByTagName('footer')[0];
@@ -60,3 +61,58 @@ checkbox.addEventListener('change', () => {
   }
 });
 
+function imprimirUltimaNoticia() {
+  // Obtener los últimos 3 elementos del arreglo
+  const ultimaNoticia = news.slice(-1);
+
+  // Crear un string HTML con el contenido de las últimas noticias
+  const noticiaHTML = ultimaNoticia.map(noticia => {
+    return `
+      <div class="noticia">
+        <div class="hr">
+          <hr>
+        </div>
+        <div class="preview">
+          <h3>${noticia.motivo}</h3>
+          <a href="${noticia.href}">
+            <img src="${noticia.imagenSrc}" alt="${noticia.imagenAlt}">
+            <h2>${noticia.titulo}</h2>
+            <p>${noticia.descripcion}</p>
+          </a>
+        </div>
+      </div>
+    `;
+  }).join('');
+
+  // Obtener el elemento con la clase "noticias" y asignarle el contenido HTML
+  const noticiasEl = document.querySelector('.ultimo');
+  noticiasEl.innerHTML = noticiaHTML;
+}
+
+function imprimirNoticiasPrevias() {
+  const ultimasNoticias = news.slice(-3, -1); // Obtenemos las dos últimas noticias
+
+  const noticiaHTML = ultimasNoticias.map(noticia => {
+    return `
+      <div class="previas">
+        <div class="hr">
+          <hr>
+        </div>
+        <div class="preview">
+          <h3>${noticia.motivo}</h3>
+          <a href="${noticia.href}">
+            <img src="${noticia.imagenSrc}" alt="${noticia.imagenAlt}">
+            <h2>${noticia.titulo}</h2>
+          </a>
+        </div>
+      </div>
+    `;
+  }).join('');
+
+  // Obtener el elemento con la clase "noticias" y asignarle el contenido HTML
+  const noticiasEl = document.querySelector('.anteriores');
+  noticiasEl.innerHTML = noticiaHTML;
+}
+
+imprimirUltimaNoticia();
+imprimirNoticiasPrevias();
