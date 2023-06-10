47913ee1d89deb00606d5cc7eedb4d2e48560972
tobi <noggnzzz@gmail.com>
2023-06-10 10:00:36 -0500

    Mapa base listo. El zoom inicial está bien en formato pc, sin embargo, hay que arreglarlo en la vista móvil

--- geoportal/js.js
+++ geoportal/js.js
@@ -1,19 +1,36 @@
-const logoContainer = document.querySelector('.logo-container');
-const logo = document.getElementsByClassName('logo-container')[0];
+const toggleBtn = document.querySelector('.toggle-btn');
+const svgIcon = document.querySelector('.icon');
+const iconInitial = document.querySelector('.icon-initial');
+const iconClose = document.querySelector('.icon-close');
+const checkbox = toggleBtn.querySelector('input[type="checkbox"]');
+const content = document.getElementsByClassName('menu-content')[0];
+const menu = document.querySelector('.menu');
 
-function beat() {
-  logoContainer.classList.add('beating');
-  setTimeout(() => {
-    logoContainer.classList.remove('beating');
-  }, 300);
-}
-setInterval(beat, 1000);
 
-setTimeout(() => {
-  logo.style.transition = 'opacity 1s ease-in-out';
-  logo.style.opacity = 0;
-}, 4000);
 
-setTimeout(() => {
-  logo.style.display = 'none';
-}, 5000);
+
+checkbox.addEventListener('change', () => {
+  if (checkbox.checked) {
+    iconInitial.style.display = 'none';
+    iconClose.style.display = 'block';
+    content.style.transform = 'translateX(0)';
+    
+  } else {
+    iconInitial.style.display = 'block';
+    iconClose.style.display = 'none';
+    content.style.transform = 'translateX(-100%)';
+    
+  }
+});
+
+
+var boundary = [-81.3899688720703, -18.4412956237793, -68.5886001586914, 0.0298568718135357];
+
+var map = L.map('map', {
+    center: [(boundary[1] + boundary[3]) / 2, (boundary[0] + boundary[2]) / 2],
+    zoom: 7,
+});
+
+L.tileLayer('http://a.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png', {
+    maxZoom: 10,
+}).addTo(map);
